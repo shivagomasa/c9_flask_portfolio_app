d4c1873f4a704578e5198435cbb4bf58ac27fff9
shivagomasa <38106695+shivagomasa@users.noreply.github.com>
2018-08-23 18:05:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	new_name = str(name) + 'good to have you'
+	new_name = str(name) + ' good to have you '
 	return render_template('index.html', name=new_name)
 
 
